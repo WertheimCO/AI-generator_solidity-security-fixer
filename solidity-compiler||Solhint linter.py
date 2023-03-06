432bace8ef062cf6e3344cf3eecdcc5b3c27add5
Samuel Wertheim <18444103+WertheimCO@users.noreply.github.com>
2023-03-05 23:22:44 -0500

    Create solidity-compiler||Solhint linter.py
    Once you have installed the dependencies, you can replace PATH_TO_CONTRACT with the path to your Solidity smart contract and run the script to identify coding issues. Note that this script is a simple example and does not use machine learning or other AI techniques to identify issues.

---
+++ solidity-compiler||Solhint linter.py
@@ -0,0 +1,7 @@
+npm install -g solc
+npm install -g solhint
+
+//Once you have installed the dependencies, you can replace PATH_TO_CONTRACT with the path to your Solidity smart contract and run the script to identify coding issues. Note that this script is a simple example and does not use machine learning or other AI techniques to identify issues.
+
+
+
